f8cfdca9e83e9f8359a1e2126b38c40c883273f5
Renata Cruz <39519638+reenaataacruuz@users.noreply.github.com>
2022-04-07 18:47:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,11 +2,26 @@ let btnPhrases = document.querySelector('button')
 let pPhrases = document.querySelector('p')
 
 let phrases = [ 
-  'Frase 1',
-  'Frase 2',
-  'Frase 3',
-  'Frase 4',
-  'Frase 5'
+  '"Eu te amo porque sua presença vale ouro."',
+  '"Eu te amo porque você me faz bem."',
+  '"Eu te amo porque você me apoia nos meus sonhos."',
+  '"Eu te amo porque você sempre esteve comigo."',
+  '"Eu te amo porque você me aprecia."',
+  '"Uma das melhores lembraças é aquele dogão do calçadão de osasco"',
+  '"Uma das melhores lembraças foi aquela vez que a enchente do Rochdale levou a gente"',
+  '"Uma das melhores lembraças são sempre as que eu criei na minha mente sobre nós."',
+  '"Shinedown - If You Only Knew"',
+  '"Shinedown - I Will Follow You"',
+  '"Killswitch Engage - The End Of Heartache"',
+  '"Killswitch Engage - My Curse"',
+  '"3 Doors Down - Here Without You"',
+  '"All That Remains - What If I Was Nothing"',
+  '"Disturbed - You are Mine"',
+  '"Fresno - Diga, Parte 2"',
+  '"Fresno - Acordar"',
+  '"Fresno - Desde Quando Você se Foi"',
+  '"One ok Rock - Wherever You Are"'
+	
 ]
 
 function loadPhrase(min, max){
